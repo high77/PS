c9bf2811bbb0bc3b3396316a70020852305020d3
hyeokil <95341353+high77@users.noreply.github.com>
2025-02-04 16:45:35 +0900

    [Silver III] Title: 카드 문자열, Time: 272 ms, Memory: 35988 KB -BaekjoonHub

---
+++ 백준/Silver/13417. 카드 문자열/카드 문자열.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder ans  = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            String[] input = br.readLine().split(" ");
+            Deque<Character> dq = new ArrayDeque<>();
+            char tmp = input[0].charAt(0);
+            dq.add(tmp);
+            for (int i =1 ; i < N ; i++) {
+                char nowC = input[i].charAt(0);
+                if (dq.peek()> nowC) {
+                    tmp = dq.peek();
+                    dq.addFirst(nowC);
+                }
+                else if (dq.peek()==nowC) {
+                    if (nowC<=tmp) dq.addFirst(nowC);
+                    else dq.add(nowC);
+                }
+                else dq.add(nowC);
+            }
+            while (!dq.isEmpty()) ans.append(dq.removeFirst());
+            ans.append('\n');
+        }
+        System.out.println(ans);
+    }
+}
+
