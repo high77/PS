1a2a13854a93125c4a4712a44b19764873402d86
hyeokil <95341353+high77@users.noreply.github.com>
2025-02-05 17:31:55 +0900

    [Gold IV] Title: 합이 0, Time: 188 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -8,33 +8,12 @@ public class Main {
         long ans = 0;
         int N = Integer.parseInt(br.readLine());
         int[] list = new int[N];
+        int[] cnt = new int[60001];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) list[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(list);
-        for (int i = 0; i < N-2; i++) {
-            if ((list[i]<0 && list[N-1]<0) || (list[i]>0 && list[N-1]>0)) break;
-            int s  = i+1 , e = N-1;
-            while (s<e){
-                int V = list[i] + list[s] + list[e];
-                if (V==0){
-                    if (list[s]==list[e]) {
-                        ans += (e-s+1)*(e-s)/2;
-                        break;
-                    }
-                    int l = 1,r = 1;
-                    while (s+1<e && list[s]==list[s+1]) {
-                        l++;
-                        s++;
-                    }
-                    while (e-1>s && list[e]==list[e-1]) {
-                        r++;
-                        e--;
-                    }
-                    ans+=l*r;
-                }
-                if (V>0) e--;
-                else  s++;
-            }
+        for (int i=0;i<N;i++){
+            ans+=cnt[30000-list[i]];
+            for (int j=0;j<i;j++)cnt[30000+list[i]+list[j]]++;
         }
         System.out.println(ans);
     }
