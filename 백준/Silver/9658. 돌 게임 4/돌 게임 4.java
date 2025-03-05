8a01e1122fb610571845b01cd2fd0602d51aa38a
hyeokil <95341353+high77@users.noreply.github.com>
2025-03-05 13:26:49 +0900

    [Silver II] Title: 돌 게임 4, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder ans = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((N%7==1||N%7==3)?"CY":"SK");
+    }
+}
