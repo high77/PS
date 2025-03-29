b691c5c78833ec9f26c4295ea0bbb8902c8c1f66
hyeokil <95341353+high77@users.noreply.github.com>
2025-03-29 23:21:19 +0900

    [Bronze III] Title: Viva la Diferencia, Time: 1068 ms, Memory: 984 KB -BaekjoonHub

--- 백준/Bronze/4084. Viva la Diferencia/Viva la Diferencia.go
+++ 백준/Bronze/4084. Viva la Diferencia/Viva la Diferencia.go
@@ -1,31 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"math"
+    "fmt"
+    "math"
 )
 
 func main() {
-	var a, b, c, d int
-	var ans int
-	for {
-		fmt.Scan(&a, &b, &c, &d)
-		if a == 0 && b == 0 && c == 0 && d == 0 {
-			break
-		}
-		ans = 0
-		for {
-			if a == b && b == c && c == d {
-				break
-			}
-			ans++
-			tmp := a
-			a = int(math.Abs(float64(a - b)))
-			b = int(math.Abs(float64(b - c)))
-			c = int(math.Abs(float64(c - d)))
-			d = int(math.Abs(float64(d - tmp)))
-		}
-		fmt.Println(ans)
-	}
-
+    for {
+        var a, b, c, d, ans int
+        fmt.Scan(&a, &b, &c, &d)
+        if a|b|c|d == 0 { 
+            break
+        }
+        
+        for a != b || b != c || c != d {
+            ans++
+            a, b, c, d = int(math.Abs(float64(a-b))), int(math.Abs(float64(b-c))), int(math.Abs(float64(c-d))), int(math.Abs(float64(d-a)))
+        }
+        fmt.Println(ans)
+    }
 }
