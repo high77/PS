643e168afb611076069362bc1902bb1ab637276b
hyeokil <95341353+high77@users.noreply.github.com>
2025-04-10 09:20:36 +0900

    [Gold V] Title: 하노이 탑, Time: 208 ms, Memory: 19296 KB -BaekjoonHub

---
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/big"
+	"os"
+	"strings"
+)
+
+var ans strings.Builder
+
+func hanoi(n int, s int, e int) {
+	if n == 0 {
+		return
+	}
+	hanoi(n-1, s, (6 - s - e))
+	ans.WriteString(fmt.Sprintf("%d %d\n", s, e))
+	hanoi(n-1, (6 - e - s), e)
+}
+
+func main() {
+	br := bufio.NewReader(os.Stdin)
+	bw := bufio.NewWriter(os.Stdout)
+	defer bw.Flush()
+	var N int
+	fmt.Fscan(br, &N)
+
+	n := new(big.Int).Lsh(big.NewInt(1), uint(N))
+	n.Sub(n, big.NewInt(1))
+	ans.WriteString(n.String())
+
+	if N < 21 {
+		ans.WriteString("\n")
+		hanoi(N, 1, 3)
+	}
+	bw.WriteString(ans.String())
+}
