9e5bbb5e381e6dbf36e31fb815573388993c0217
hyeokil <95341353+high77@users.noreply.github.com>
2025-04-10 09:30:12 +0900

    [Gold V] Title: 하노이 탑, Time: 124 ms, Memory: 884 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.go
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.go
@@ -5,34 +5,37 @@ import (
 	"fmt"
 	"math/big"
 	"os"
-	"strings"
 )
 
-var ans strings.Builder
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
 
-func hanoi(n int, s int, e int) {
-	if n == 0 {
-		return
+	var n int
+	fmt.Fscanln(reader, &n)
+
+	// 전체 이동 횟수 계산
+	totalMoves := new(big.Int).Lsh(big.NewInt(1), uint(n)) // 2^n
+	totalMoves.Sub(totalMoves, big.NewInt(1))             // 2^n - 1
+
+	fmt.Fprintln(writer, totalMoves.String())
+	
+	// n이 20 이하일 때만 하노이 진행
+	if n <= 20 {
+		hanoiProgress(n, 1, 3, writer)
 	}
-	hanoi(n-1, s, (6 - s - e))
-	ans.WriteString(fmt.Sprintf("%d %d\n", s, e))
-	hanoi(n-1, (6 - e - s), e)
 }
 
-func main() {
-	br := bufio.NewReader(os.Stdin)
-	bw := bufio.NewWriter(os.Stdout)
-	defer bw.Flush()
-	var N int
-	fmt.Fscanln(br, &N)
-
-	n := new(big.Int).Lsh(big.NewInt(1), uint(N))
-	n.Sub(n, big.NewInt(1))
-	ans.WriteString(n.String())
-
-	if N < 21 {
-		ans.WriteString("\n")
-		hanoi(N, 1, 3)
+// 하노이 이동 경로를 출력하는 함수
+func hanoiProgress(n, from, to int, writer *bufio.Writer) {
+	if n == 1 {
+		fmt.Fprintln(writer, from, to)
+		return
 	}
-	bw.WriteString(ans.String())
+	// 보조 기둥 계산
+	auxiliary := 6 - from - to
+	hanoiProgress(n-1, from, auxiliary, writer)
+	fmt.Fprintln(writer, from, to)
+	hanoiProgress(n-1, auxiliary, to, writer)
 }
