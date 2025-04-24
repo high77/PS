6dcc3ad755ca19f4e01882153f33d8a250ab9d4d
hyeokil <95341353+high77@users.noreply.github.com>
2025-04-24 13:22:52 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 836 ms, Memory: 163724 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder ans = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken())
+                , P = Integer.parseInt(st.nextToken()), ans=0;
+        Stack<Integer>[] stack = new Stack[N+1];
+        for (int i = 0; i <= N; i++) stack[i] = new Stack<>();
+        for (int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken())
+                    , b = Integer.parseInt(st.nextToken());
+            while (!stack[a].empty() && stack[a].peek()>b){
+                if (stack[a].peek() > b) {
+                    stack[a].pop();
+                    ans++;
+                }
+            }
+            if (!stack[a].empty() && stack[a].peek() == b) continue;
+            stack[a].push(b);
+            ans++;
+        }
+        System.out.println(ans);
+    }
+}
