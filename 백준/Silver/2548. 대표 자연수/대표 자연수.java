7e47c9f1561461973e057da7ceecfe35272c4e49
hyeokil <95341353+high77@users.noreply.github.com>
2025-04-25 15:20:56 +0900

    [Silver III] Title: 대표 자연수, Time: 120 ms, Memory: 15300 KB -BaekjoonHub

--- 백준/Silver/2548. 대표 자연수/대표 자연수.java
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.java
@@ -10,16 +10,6 @@ public class Main{
         int[] list = new int[N];
         for(int i = 0; i < N; i++) list[i] = Integer.parseInt(st.nextToken());
         Arrays.sort(list);
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < N; i++) {
-            int res = 0;
-            if (i>1 && list[i] == list[i-1]) continue;
-            for (int j = 0; j < N; j++) res += Math.abs(list[i]-list[j]);
-            if (res < min) {
-                min = res;
-                ans = list[i];
-            }
-        }
-        System.out.println(ans);
+        System.out.println(list[(N-1)/2]);
     }
 }
