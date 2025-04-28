0bbf453ca7056cb430ead995f85ddfbb5418df24
hyeokil <95341353+high77@users.noreply.github.com>
2025-04-28 17:09:38 +0900

    [Gold III] Title: 음악프로그램, Time: 28 ms, Memory: 992 KB -BaekjoonHub

---
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var in = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	in.Scan()
+	r := 0
+	for _, c := range in.Bytes() {
+		r *= 10
+		r += int(c - '0')
+	}
+	return r
+}
+
+func main() {
+	in.Split(bufio.ScanWords)
+	N, M := nextInt(), nextInt()
+	list := make([]int, N+1)
+	arr := make([][]int, N+1)
+	ans := make([]int, 0)
+
+	for i := 0; i < M; i++ {
+		k, prev := nextInt(), nextInt()
+		for j := 1; j < k; j++ {
+			now := nextInt()
+			arr[prev] = append(arr[prev], now)
+			list[now]++
+			prev = now
+		}
+	}
+
+	visit := make([]bool, N+1)
+	q := make([]int, 0)
+
+	for i := 1; i <= N; i++ {
+		if list[i] == 0 {
+			q = append(q, i)
+		}
+	}
+
+	for len(q) > 0 {
+		now := q[0]
+		if visit[now] {
+			fmt.Println(0)
+			return
+		}
+		ans = append(ans, now)
+		visit[now] = true
+		for _, i := range arr[now] {
+			list[i]--
+			if list[i] == 0 {
+				q = append(q, i)
+			}
+		}
+		q = q[1:]
+	}
+
+	if len(ans) == N {
+		for _, i := range ans {
+			fmt.Println(i)
+		}
+	} else {
+		fmt.Println(0)
+	}
+
+}
