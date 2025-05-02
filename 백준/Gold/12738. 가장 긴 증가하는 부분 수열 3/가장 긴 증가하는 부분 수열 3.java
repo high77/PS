6dedbdfc9acd4acb8378fa5ee76eec80633fcc59
hyeokil <95341353+high77@users.noreply.github.com>
2025-05-02 13:35:02 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 548 ms, Memory: 182060 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] list = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) list[i] = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> LIS = new ArrayList<>();
+        LIS.add(list[0]);
+        for (int i = 1; i < N; i++) {
+            int n = list[i];
+            if (LIS.get(LIS.size()-1)<n) LIS.add(n);
+            else {
+                int s = 0, e = LIS.size()-1;
+                while (s<e) {
+                    int mid = (s+e)/2;
+                    if (LIS.get(mid)>=n) e = mid;
+                    else s = mid+1;
+                }
+                LIS.set(e,n);
+            }
+        }
+        System.out.println(LIS.size());
+    }
+}
