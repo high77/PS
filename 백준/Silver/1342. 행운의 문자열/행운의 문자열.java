47d9a7ac413b07ef58cd794481985597307237f2
hyeokil <95341353+high77@users.noreply.github.com>
2025-06-17 00:14:54 +0900

    [Silver I] Title: 행운의 문자열, Time: 364 ms, Memory: 11632 KB -BaekjoonHub

--- 백준/Silver/1342. 행운의 문자열/행운의 문자열.java
+++ 백준/Silver/1342. 행운의 문자열/행운의 문자열.java
@@ -3,19 +3,10 @@ import java.io.*;
 public class Main{
     static int[] alphabet = new int[27];
     static int res;
-    static int L;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        L = s.length();
-        for (int i = 0; i < s.length(); i++)alphabet[s.charAt(i)-'a']++;
+    static int N;
 
-        dfs(0,' ');
-        System.out.println(res);
-    }
-
-    public static void dfs(int index,char pre){
-        if (index== L){
+    static void dfs(int k,int pre){
+        if (k == N){
             res++;
             return;
         }
@@ -23,12 +14,24 @@ public class Main{
         for (int i = 0; i < 27; i++){
             if (alphabet[i]==0)continue;
 
-            if (pre != (char) (i+'a')){
+            if (pre != i+'a'){
                 alphabet[i]--;
-                dfs(index+1, (char)(i+'a'));
+                dfs(k +1, i+'a');
                 alphabet[i]++;
             }
 
         }
     }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        N = s.length();
+        for (int i = 0; i < s.length(); i++)alphabet[s.charAt(i)-'a']++;
+
+        dfs(0,0);
+        System.out.println(res);
+    }
+
+
 }
