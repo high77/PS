88af9010a1da33bd524f377b80d8da70ab03e3d9
hyeokil <95341353+high77@users.noreply.github.com>
2025-06-23 22:02:33 +0900

    [Silver IV] Title: 별 찍기 - 23, Time: 72 ms, Memory: 13020 KB -BaekjoonHub

--- 백준/Silver/13015. 별 찍기 － 23/별 찍기 － 23.java
+++ 백준/Silver/13015. 별 찍기 － 23/별 찍기 － 23.java
@@ -3,47 +3,47 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        StringBuilder ans = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
         int i, j;
 
         for (i = 1; i <= 2 * n - 1; i++) {
             if (i == 1 || i == (n + (n - 1))) { // 첫 줄과 마지막 줄
-                for (j = 1; j <= n; j++) sb.append("*");
-                for (j = 1; j <= (n - 1) + (n - 2); j++) sb.append(" ");
-                for (j = 1; j <= n; j++) sb.append("*");
+                for (j = 1; j <= n; j++) ans.append("*");
+                for (j = 1; j <= (n - 1) + (n - 2); j++) ans.append(" ");
+                for (j = 1; j <= n; j++) ans.append("*");
             } else {
                 if (i < n) {
-                    for (j = 1; j <= i - 1; j++) sb.append(" ");
-                    sb.append("*");
-                    for (j = 1; j <= n - 2; j++) sb.append(" ");
-                    sb.append("*");
-                    for (j = 1; j <= (n - 1) + (n - 2) - 2 * (i - 1); j++) sb.append(" ");
-                    sb.append("*");
-                    for (j = 1; j <= n - 2; j++) sb.append(" ");
-                    sb.append("*");
+                    for (j = 1; j <= i - 1; j++) ans.append(" ");
+                    ans.append("*");
+                    for (j = 1; j <= n - 2; j++) ans.append(" ");
+                    ans.append("*");
+                    for (j = 1; j <= (n - 1) + (n - 2) - 2 * (i - 1); j++) ans.append(" ");
+                    ans.append("*");
+                    for (j = 1; j <= n - 2; j++) ans.append(" ");
+                    ans.append("*");
                 } else if (i > n) {
-                    for (j = 1; j <= n - (i % n) - 1; j++) sb.append(" ");
-                    sb.append("*");
-                    for (j = 1; j <= n - 2; j++) sb.append(" ");
-                    sb.append("*");
-                    for (j = 1; j <= 2 * (i % n) - 1; j++) sb.append(" ");
-                    sb.append("*");
-                    for (j = 1; j <= n - 2; j++) sb.append(" ");
-                    sb.append("*");
-                } else { 
-                    for (j = 1; j <= i - 1; j++) sb.append(" ");
-                    sb.append("*");
-                    for (j = 1; j <= n - 2; j++) sb.append(" ");
-                    sb.append("*");
-                    for (j = 1; j <= n - 2; j++) sb.append(" ");
-                    sb.append("*");
+                    for (j = 1; j <= n - (i % n) - 1; j++) ans.append(" ");
+                    ans.append("*");
+                    for (j = 1; j <= n - 2; j++) ans.append(" ");
+                    ans.append("*");
+                    for (j = 1; j <= 2 * (i % n) - 1; j++) ans.append(" ");
+                    ans.append("*");
+                    for (j = 1; j <= n - 2; j++) ans.append(" ");
+                    ans.append("*");
+                } else {
+                    for (j = 1; j <= i - 1; j++) ans.append(" ");
+                    ans.append("*");
+                    for (j = 1; j <= n - 2; j++) ans.append(" ");
+                    ans.append("*");
+                    for (j = 1; j <= n - 2; j++) ans.append(" ");
+                    ans.append("*");
                 }
             }
-            sb.append("\n");
+            ans.append("\n");
         }
 
-        System.out.print(sb.toString());
+        System.out.print(ans);
     }
 }
