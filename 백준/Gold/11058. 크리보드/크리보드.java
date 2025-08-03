ea5b360356551c4e3da55f1f161e3597fd658c9b
hyeokil <95341353+high77@users.noreply.github.com>
2025-08-03 21:31:57 +0900

    [Gold V] Title: 크리보드, Time: 60 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Gold/11058. 크리보드/크리보드.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[N + 1];
+        for (int i = 1; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if(i>6){
+                for(int j=3;j<6;j++)dp[i] = Math.max(dp[i], dp[i-j] * (j-1));
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
