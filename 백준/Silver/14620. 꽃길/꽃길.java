c0d03af7aee225db12af5b7edc99f1197415dd7a
hyeokil <95341353+high77@users.noreply.github.com>
2025-08-26 23:02:03 +0900

    [Silver II] Title: 꽃길, Time: 116 ms, Memory: 15792 KB -BaekjoonHub

---
+++ 백준/Silver/14620. 꽃길/꽃길.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
+    static int ans = Integer.MAX_VALUE, N;
+    static int[][] A;
+    static boolean[][] V;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        A = new int[N][N];
+        V = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) A[i][j] = Integer.parseInt(st.nextToken());
+        }
+        DFS(0, 0);
+        System.out.println(ans);
+    }
+
+    static void DFS(int depth, int sum){
+        // 백트래킹 효율, 더이상 탐색할 필요가 없다면 연산을 종료한다.
+        if(sum >= ans) return;
+
+        if(depth == 3){
+            ans = Math.min(sum, ans);
+        }else{
+            for(int i=1; i<N-1; i++){
+                for(int j=1; j<N-1; j++){
+                    if(!V[i][j] && checkVisit(i, j)){
+                        int cost = sumCost(i, j);
+                        DFS(depth+1, sum+cost);
+                        clearVisit(i, j);
+                    }
+                }
+            }
+        }
+    }
+
+    static boolean checkVisit(int x, int y){
+        for(int i=0; i<4; i++){
+            if(V[x+dx[i]][y+dy[i]]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static int sumCost(int x, int y){
+        int sum = A[x][y];
+        V[x][y] = true;
+        for(int i=0; i<4; i++){
+            sum += A[x+dx[i]][y+dy[i]];
+            V[x+dx[i]][y+dy[i]] = true;
+        }
+        return sum;
+    }
+
+    static void clearVisit(int x, int y){
+        for(int i=0; i<4; i++){
+            V[x+dx[i]][y+dy[i]] = false;
+        }
+        V[x][y] = false;
+    }
+}
