5d77dd3cd9435599eea0a5b6fbc98601057ba34f
hyeokil <95341353+high77@users.noreply.github.com>
2025-11-14 15:22:14 +0900

    [Bronze I] Title: 선 그리기, Time: 80 ms, Memory: 11956 KB -BaekjoonHub

--- 백준/Bronze/16396. 선 그리기/선 그리기.java
+++ 백준/Bronze/16396. 선 그리기/선 그리기.java
@@ -2,16 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int N = read();
 
         int[] diff = new int[10002];
 
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken())
-                    , y = Integer.parseInt(st.nextToken());
+            int x = read(), y = read();
             if (x > y) { int t = x; x = y; y = t; }
 
             diff[x]++;   
