22123f39c325ef30f3c36991879e88b15040154f
hyeokil <95341353+high77@users.noreply.github.com>
2026-04-07 20:47:56 +0900

    [Silver V] Title: Triangles of a Square, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/32842. Triangles of a Square/Triangles of a Square.java
+++ 백준/Silver/32842. Triangles of a Square/Triangles of a Square.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        if (!sc.hasNextInt()) return;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken()), y1 = Integer.parseInt(st.nextToken()),
+                 x2 = Integer.parseInt(st.nextToken()), y2 = Integer.parseInt(st.nextToken());
 
         int corners = 0;
 
